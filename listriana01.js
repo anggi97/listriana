a2d3f3362a5a54c2825ca874068d1ad9b3d8b1a9
anggi97 <anggiwijaya130497@gmail.com>
2019-06-04 01:45:51 +0700

    Update listriana01.js

--- listriana01.js
+++ listriana01.js
@@ -1,13 +1,1 @@
-<div id='teaser2' style='width:autopx; height:autopx; text-align:left; display:scroll;position:fixed; top:110px;left:40px; z-index:20000'>
-<div style='color: red;font-size: 12px;margin-left: -7px;'> 
-   [x] <script>
-        function myfunction() {
-     document.getElementById(&#39;teaser2&#39;).style.display = &#39;none&#39;;
- }
-var delay = 600;
-</script>
-<a href='#' id='close-teaser' onclick='setTimeout( myfunction, delay);'> 
-<left style='margin-left: 20px;'><img border='0' data-original-height='40' data-original-width='120' height='40px' src='https://4.bp.blogspot.com/-O4AGw2r1S9g/W_q1fY_xv6I/AAAAAAAAAHM/ZZAjB1A7xWccqJlsaw7sJwnvcVFXKV4HwCLcBGAs/s1600/Screen%2BShot%2B2018-09-21%2Bat%2B19.32.29.png' style='height: auto;max-width: 100%;' width='120px'/></left>
-</a></div>
-<center><img border='0' data-original-height='330' data-original-width='600' height='460px' src='https://1.bp.blogspot.com/-cyhO78tGmZI/XPUJ8l_aYSI/AAAAAAAAGc0/fCRUrGKTiWwxS1WKxADo5ri9kg1i9qiEgCLcBGAs/s1600/20190603_170100.jpg' width='300px'/></center>
-</div>
+#listriana
